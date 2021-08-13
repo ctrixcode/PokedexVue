404a04d5e1424c42f42ce5bac6f77fb7c7684b44
Chandrashekhar Kachawa <42296139+mcctrix@users.noreply.github.com>
2021-08-13 19:47:19 +0530

    Create Storing_Data.vue

---
+++ components/Storing_Data.vue
@@ -0,0 +1,36 @@
+<template>
+    <p>hi</p>
+</template>
+<script>
+export default {
+    methods: {
+        Pokedex_download(poke_num) {
+            fetch("https://pokeapi.co/api/v2/pokemon/" + poke_num)
+            .then(res => {
+                if(res.ok)
+                {
+                    return res.json();
+                }
+                else {
+                    return "Error";
+                }
+            }).then((data) => {
+                this.$store.state.Poke_database.push(() => {
+                    return data
+                }) 
+            });
+        },
+        pokemon() {
+        let x = 1;
+        while( x < 10 ) {
+        this.Pokedex_download(x);
+        x++;
+        }
+        }
+    },
+    beforeMount() {
+        
+    },
+}
+
+</script>
